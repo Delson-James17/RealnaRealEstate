8e6e856d3c8202b3c16529df6310cc9429fe934e
mardanniel <mardannielabarondoginturo@gmail.com>
2023-05-12 17:25:28 +0800

    add repository for every model

---
+++ Real_Estate/Repository/SalesOrRents/ISaleOrRentRepository.cs
@@ -0,0 +1,9 @@
+﻿using Real_Estate.Models;
+
+namespace Real_Estate.Repository.SalesOrRents
+{
+    public interface ISaleOrRentRepository
+    {
+        Task<List<SaleorRentModel>> GetAllSaleOrRent();
+    }
+}
